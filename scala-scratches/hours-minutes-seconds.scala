02a50f37847898debf9da2ff5b5111db8452988b
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-07-25 23:55:20 +0100

    watching an akka video tutorial

--- scala-scratches/hours-minutes-seconds.scala
+++ scala-scratches/hours-minutes-seconds.scala
@@ -1,7 +1,7 @@
-val R = """.*(\d\d?):(\d\d?):(\d\d?).*""".r
+val R = """^(\d\d?):(\d\d?):(\d\d?).*""".r
 case class HMS(h:Int, m: Int, s:Int);
 val total = scala.io.Source.fromFile("./data/hms.txt").getLines.collect {
-  case R(hh,mm,ss) => HMS(hh.toInt, mm.toInt, ss.toInt) 
+  case R(hh,mm,ss) => HMS(hh.toInt, mm.toInt, ss.toInt)
 }.foldLeft(0){(acc, hms) => acc+hms.s+hms.m*60+hms.h*3600}
 def normaliseTotal(s:Int):HMS = {
   val h = s / 3600
